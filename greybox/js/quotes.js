9e4bc8d65edfaef65e3f8b8db374fc058f4c5b86
joeld <joeldombek@joel-dombek-3.local>
2016-04-12 06:49:43 -0400

    adding quotes

--- greybox/js/quotes.js
+++ greybox/js/quotes.js
@@ -46,21 +46,22 @@ $(document).ready(function() {
 	cycleThru();
 });
 
-$(document).ready(function() {
-	$('.nav-toggle').click(function(){
-		//get collapse content selector
-		var collapse_content_selector = $(this).attr('href');
 
-		//make the collapse content to be shown or hide
-		var toggle_switch = $(this);
-		$(collapse_content_selector).toggle(function(){
-			if($(this).css('display')=='none'){
-				//change the button label to be 'Show'
-				toggle_switch.html('<i class="material-icons">add</i>');
-			}else{
-				//change the button label to be 'Hide'
-				toggle_switch.html('<i class="material-icons">clear</i>');
-			}
-		});
-	});
-});
+
+
+
+// http://api.jquery.com/slideToggle/
+$('.quote-toggle').click(function(){
+  //get collapse content selector
+  var collapse_content_selector = $(this).attr('href');
+
+  //make the collapse content to be shown or hide
+  var toggle_switch = $(this);
+    $(collapse_content_selector).slideToggle(function(){
+      if($(this).css('display')==='none'){
+        toggle_switch.html('<i class="material-icons">add</i>');
+      }else{
+        toggle_switch.html('<i class="material-icons">clear</i>');
+      }
+    });
+  });
