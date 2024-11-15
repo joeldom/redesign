3e4775e6723b2f5d4ce3b3fadb91436fb2e6a57b
Joel <dombekj890@gmail.com>
2024-11-14 23:17:42 -0500

    prompt format + styles

---
+++ js/gtag.js
@@ -0,0 +1,11 @@
+//   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+//   <!-- BASE Google tag (gtag.js) -->
+//   <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-VCY50JK5BC"></script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-D9GLG41WK0');
