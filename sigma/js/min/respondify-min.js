5ec6475e4238220c9d1212c8f596015012eb8df5
Joel <dombekj890@gmail.com>
2026-03-17 02:05:28 -0400

    archive

--- sigma/js/min/respondify-min.js
+++
@@ -1,1 +0,0 @@
-function respondify(){$('iframe[src*="embed.spotify.com"]').each(function(){$(this).css("width",$(this).parent(1).css("width")),$(this).attr("src",$(this).attr("src"))})}$(document).ready(function(){respondify()}),$(window).resize(function(){respondify()});
